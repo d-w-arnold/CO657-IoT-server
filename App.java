0f58b42235ca59d83f9ee4a8b2c7f99577688659
David W. Arnold <d.w.arnold.gm@gmail.com>
2019-11-19 14:17:32 +0000

    Added RXTX code from Arduino, for SerialTest/Communicator class

--- App.java
+++ App.java
@@ -12,7 +12,23 @@ public class App
 {
     public static void main(String[] args)
     {
-        light();
+//        System.out.println(System.getProperty("java.library.path"));
+
+        Communicator main = new Communicator();
+        main.initialize();
+        Thread t = new Thread() {
+            public void run() {
+                //the following line will keep this app alive for 1000 seconds,
+                //waiting for events to occur and responding to them (printing incoming messages to console).
+                try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
+            }
+        };
+        t.start();
+        System.out.println("Started");
+
+
+
+//        light();
     }
 
     public static void light() {
