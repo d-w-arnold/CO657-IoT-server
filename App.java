34db9f0ea737da317337f7a4127a905ae8da88c4
David W. Arnold <d.w.arnold.gm@gmail.com>
2019-12-04 16:56:21 +0000

    Working setlight() method via HTTP PUT request

--- App.java
+++ App.java
@@ -1,10 +1,9 @@
-import com.fazecast.jSerialComm.SerialPort;
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.sql.*;
 import java.util.HashMap;
-import java.util.Set;
 
 /**
  * IoT Server
@@ -14,47 +13,53 @@ import java.util.Set;
  */
 public class App
 {
+    final private static String lightIPAddress = "10.150.46.108";
+    final private static String apiKey = "70617373776f7264";
     private static HashMap<String, String> bleDeviceNames = new HashMap<>();
     private static HashMap<String, String> living_roomMap = new HashMap<>();
     private static HashMap<String, String> bedroomMap = new HashMap<>();
     private static HashMap<String, String> kitchenMap = new HashMap<>();
 
-    public static void main(String[] args) throws SQLException
+    public static void main(String[] args) throws SQLException, IOException
     {
-        // Serial Port Setup
-        SerialPort commPort = SerialPort.getCommPort("/dev/tty.usbserial-A9Z2T81O");
-        commPort.setBaudRate(9600);
-        commPort.openPort();
-        commPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+//        System.out.println(getLight());
+        setLight(2);
 
-        // JDBC connection to my MySQL database
-        Connection conn = connectToDatabase();
-
-        // SELECT * FROM iot
-        assert conn != null;
-        populateMaps(conn);
-
-        // Send all BLE Device MAC addresses and BLE Device Names
-        Set<String> bleDevices = bleDeviceNames.keySet();
-        for (String bleDevice : bleDevices) {
-            String bleDeviceInfoToSend = bleDevice + "+" + bleDeviceNames.get(bleDevice) + "*";
-            commPort.writeBytes(bleDeviceInfoToSend.getBytes(), bleDeviceInfoToSend.length());
-        }
-
-        // Reading from Serial Port
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(commPort.getInputStream()))) {
-            while (true) {
-                System.out.println(in.readLine());
-            }
-        } catch (Exception e) {
-            System.err.println("** No Serial Input **");
-        }
-
-        // Close JDBC connection to my MySQL database
-        conn.close();
-
-        // Close Serial Port
-        commPort.closePort();
+//        // Serial Port Setup
+//        SerialPort commPort = SerialPort.getCommPort("/dev/tty.usbserial-A9Z2T81O");
+//        commPort.setBaudRate(9600);
+//        commPort.openPort();
+//        commPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+//
+//        // JDBC connection to my MySQL database
+//        Connection conn = connectToDatabase();
+//
+//        // SELECT * FROM iot
+//        assert conn != null;
+//        populateMaps(conn);
+//
+//        // Send all BLE Device MAC addresses and BLE Device Names
+//        Set<String> bleDevices = bleDeviceNames.keySet();
+//        for (String bleDevice : bleDevices) {
+//            String bleDeviceInfoToSend = bleDevice + "+" + bleDeviceNames.get(bleDevice) + "*";
+//            commPort.writeBytes(bleDeviceInfoToSend.getBytes(), bleDeviceInfoToSend.length());
+//        }
+//
+//        // Reading from Serial Port
+//        try (BufferedReader in = new BufferedReader(new InputStreamReader(commPort.getInputStream()))) {
+//            while (true) {
+//                System.out.println(in.readLine());
+//
+//            }
+//        } catch (Exception e) {
+//            System.err.println("** No Serial Input **");
+//        }
+//
+//        // Close JDBC connection to my MySQL database
+//        conn.close();
+//
+//        // Close Serial Port
+//        commPort.closePort();
     }
 
     private static Connection connectToDatabase()
@@ -84,16 +89,52 @@ public class App
         stmt.close();
     }
 
-//    // The Shed - example of getting the String representation of the source code at a URL
-//    public static void light()
-//    {
-//        try (BufferedReader in = new BufferedReader(new InputStreamReader(new URL("https://www.google.com").openConnection().getInputStream()))) {
-//            String line;
-//            while ((line = in.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-//    }
+    private static int getLight() throws IOException
+    {
+        // curl -H "Accept: application/json" http://10.150.46.108/api/relay/0?apikey=70617373776f7264
+        String url = "http://"+ lightIPAddress + "/api/relay/0?apikey="+ apiKey;
+        String[] command = {"curl", "-H", "Accept: application/json", url};
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        Process process = processBuilder.start();
+        int res = -1;
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                try {
+                    res = Integer.parseInt(line); // Sort this out
+                } catch (NumberFormatException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        process.destroy();
+        return res;
+    }
+
+    private static int setLight(int value) throws IOException
+    {
+        // curl -X PUT -H "Accept: application/json" http://10.150.46.108/api/relay/0 --data "apikey=70617373776f7264&value=2"
+        String parameters = "apikey="+ apiKey + "&" + "value=" + value;
+        String url = "http://"+ lightIPAddress + "/api/relay/0";
+        String[] command = {"curl", "-X", "PUT", "H", "Accept: application/json", url, "--data", parameters};
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        Process process = processBuilder.start();
+        int res = -1;
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                try {
+                    res = Integer.parseInt(line);
+                } catch (NumberFormatException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        process.destroy();
+        return res;
+    }
 }
