68ae3d3e1d35e88cda652756a62225b39240faf2
David W. Arnold <d.w.arnold.gm@gmail.com>
2019-12-07 21:31:35 +0000

    Checks for updates from database, though not pushing to IoT Device, as it would interfere with BLE scanning - IoT Device will get updates on reset, all tested

--- App.java
+++ App.java
@@ -1,4 +1,5 @@
 import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortTimeoutException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,9 +8,8 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.sql.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.Set;
 
 /**
  * IoT Server
@@ -19,15 +19,16 @@ import java.util.Set;
  */
 public class App
 {
+    final private static int newReadTimeout = 3000;
     final private static int baudRate = 9600;
     final private static String commPortPath = "/dev/tty.usbserial-A9Z2T81O";
     final private static String lightIPAddress = "10.150.46.108";
     final private static String apiKey = "70617373776f7264";
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("(dd/MM/yyyy) HH:mm:ss");
     private static HashMap<String, String> bleDeviceNames = new HashMap<>();
     private static HashMap<String, Light> living_roomMap = new HashMap<>();
     private static HashMap<String, Light> bedroomMap = new HashMap<>();
     private static HashMap<String, Light> kitchenMap = new HashMap<>();
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("(dd/MM/yyyy) HH:mm:ss");
 
     public static void main(String[] args) throws SQLException, IOException
     {
@@ -35,12 +36,11 @@ public class App
         SerialPort commPort = SerialPort.getCommPort(commPortPath);
         commPort.setBaudRate(baudRate);
         commPort.openPort();
-        commPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+        commPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, newReadTimeout, 0);
         System.out.println("** Serial Comm Port Setup Complete **");
 
         // JDBC connection to my MySQL database
         Connection conn = connectToDatabase();
-        System.out.println("** Connected to Database **");
 
         // SELECT * FROM iot
         assert conn != null;
@@ -48,8 +48,7 @@ public class App
         System.out.println("** BLE Device Info retrieved from Database **");
 
         // Send all BLE Device MAC addresses and BLE Device Names
-        Set<String> bleDevices = bleDeviceNames.keySet();
-        for (String bleDevice : bleDevices) {
+        for (String bleDevice : bleDeviceNames.keySet()) {
             String bleDeviceInfoToSend = bleDevice + "+" + bleDeviceNames.get(bleDevice) + "*";
             commPort.writeBytes(bleDeviceInfoToSend.getBytes(), bleDeviceInfoToSend.length());
         }
@@ -57,16 +56,24 @@ public class App
 
         // Reading from Serial Port
         try (BufferedReader in = new BufferedReader(new InputStreamReader(commPort.getInputStream()))) {
-            Light currentState = getLight();
+//            Light currentState = getLight();
             while (true) {
-                String bleMACAddress = in.readLine();
-                System.out.println("\nIoT Device has detected the BLE Device: " + bleMACAddress);
-                String timestamp = sdf.format(new Timestamp(System.currentTimeMillis()));
-                System.out.println(timestamp + " - Welcome home! " + bleDeviceNames.get(bleMACAddress));
-                if (Objects.equals(currentState, Light.OFF)) {
-                    if (!Objects.equals(currentState, living_roomMap.get(bleMACAddress))) {
-                        currentState = setLight(Light.TOGGLE);
-                    }
+                try {
+                    String bleMACAddress = in.readLine();
+                    System.out.println("\nIoT Device has detected the BLE Device: " + bleMACAddress);
+                    String timestamp = sdf.format(new Timestamp(System.currentTimeMillis()));
+                    System.out.println(timestamp + " - Welcome home! " + bleDeviceNames.get(bleMACAddress));
+//                if (Objects.equals(currentState, Light.OFF)) {
+//                    if (!Objects.equals(currentState, living_roomMap.get(bleMACAddress))) {
+//                        currentState = setLight(Light.TOGGLE);
+//                    }
+//                }
+                } catch (SerialPortTimeoutException se) {
+                }
+                if (requireUpdatesFromDatabase(conn)) {
+                    emptyMaps();
+                    populateMaps(conn);
+                    System.out.println("** Updated BLE Device Info retrieved from Database **");
                 }
             }
         } catch (Exception e) {
@@ -85,9 +92,12 @@ public class App
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); // Step 1: "Load" the JDBC driver
             String url = "jdbc:mysql://dragon.kent.ac.uk:3306/dwa4"; // Step 2: Establish the connection to the database
-            return DriverManager.getConnection(url, "dwa4", "i9sipin");
+            Connection conn = DriverManager.getConnection(url, "dwa4", "i9sipin");
+            System.out.println("** Connected to Database **");
+            return conn;
         } catch (Exception e) {
             System.err.println("** Cannot connect to Database **");
+            System.exit(1);
         }
         return null;
     }
@@ -105,6 +115,38 @@ public class App
         }
         rs.close();
         stmt.close();
+    }
+
+    private static boolean requireUpdatesFromDatabase(Connection conn) throws SQLException
+    {
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM iot");
+        ArrayList<String> bleDevices = new ArrayList<>();
+        while (rs.next()) {
+            String bleDevice = rs.getString("mac");
+            if (!bleDeviceNames.containsKey(bleDevice)) {
+                rs.close();
+                stmt.close();
+                return true;
+            }
+            bleDevices.add(bleDevice);
+        }
+        if (bleDevices.size() != bleDeviceNames.size()) {
+            rs.close();
+            stmt.close();
+            return true;
+        }
+        rs.close();
+        stmt.close();
+        return false;
+    }
+
+    private static void emptyMaps()
+    {
+        bleDeviceNames.clear();
+        living_roomMap.clear();
+        bedroomMap.clear();
+        kitchenMap.clear();
     }
 
     private static Light getLight() throws IOException
