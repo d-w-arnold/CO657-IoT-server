dc2eac080ab9987965ef1e8c85dce1cc32c25e6e
David W. Arnold <d.w.arnold.gm@gmail.com>
2019-12-08 00:04:00 +0000

    Still working, commented out attempted code for requesting BLE Device Info and ACK, including robustness for if smart home light is not available

--- App.java
+++ App.java
@@ -4,12 +4,17 @@ import com.fazecast.jSerialComm.SerialPortTimeoutException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.UnknownHostException;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * IoT Server
@@ -20,15 +25,21 @@ import java.util.HashMap;
 public class App
 {
     final private static int newReadTimeout = 3000;
-    final private static int baudRate = 9600;
-    final private static String commPortPath = "/dev/tty.usbserial-A9Z2T81O";
     final private static String lightIPAddress = "10.150.46.108";
     final private static String apiKey = "70617373776f7264";
     private static final SimpleDateFormat sdf = new SimpleDateFormat("(dd/MM/yyyy) HH:mm:ss");
+    private static final Pattern VALID_IPV6_PATTERN = Pattern.compile("[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]", Pattern.CASE_INSENSITIVE);
+    // Zigbee
+    final private static int baudRate = 9600;
+    final private static String commPortPath = "/dev/tty.usbserial-A9Z2T81O";
+    // Direct, no Zigbee
+//    final private static int baudRate = 115200;
+//    final private static String commPortPath = "/dev/tty.usbserial-1410";
     private static HashMap<String, String> bleDeviceNames = new HashMap<>();
     private static HashMap<String, Light> living_roomMap = new HashMap<>();
     private static HashMap<String, Light> bedroomMap = new HashMap<>();
     private static HashMap<String, Light> kitchenMap = new HashMap<>();
+    private static boolean infoSent = false;
 
     public static void main(String[] args) throws SQLException, IOException
     {
@@ -48,27 +59,44 @@ public class App
         System.out.println("** BLE Device Info retrieved from Database **");
 
         // Send all BLE Device MAC addresses and BLE Device Names
-        for (String bleDevice : bleDeviceNames.keySet()) {
-            String bleDeviceInfoToSend = bleDevice + "+" + bleDeviceNames.get(bleDevice) + "*";
-            commPort.writeBytes(bleDeviceInfoToSend.getBytes(), bleDeviceInfoToSend.length());
-        }
-        System.out.println("** Sent BLE Device Info to IoT Device **");
+        sendAll(commPort);
 
         // Reading from Serial Port
         try (BufferedReader in = new BufferedReader(new InputStreamReader(commPort.getInputStream()))) {
-//            Light currentState = getLight();
+            String bleMACAddress;
             while (true) {
                 try {
-                    String bleMACAddress = in.readLine();
-                    System.out.println("\nIoT Device has detected the BLE Device: " + bleMACAddress);
-                    String timestamp = sdf.format(new Timestamp(System.currentTimeMillis()));
-                    System.out.println(timestamp + " - Welcome home! " + bleDeviceNames.get(bleMACAddress));
-//                if (Objects.equals(currentState, Light.OFF)) {
-//                    if (!Objects.equals(currentState, living_roomMap.get(bleMACAddress))) {
-//                        currentState = setLight(Light.TOGGLE);
-//                    }
-//                }
+                    if (commPort.getInputStream().available() > 0) {
+                        String readLine = in.readLine();
+//                        System.out.println("Readline from IoT Device : " + readLine);
+                        if (readLine == null) {
+                            continue;
+                        }
+//                        if (readLine.contains("info")) {
+//                            sendAll(commPort);
+//                        }
+                        if (infoSent) {
+                            bleMACAddress = checkIsIPv6MACAddress(readLine);
+                            if (!bleMACAddress.equals("")) {
+                                System.out.println("\nIoT Device has detected the BLE Device: " + bleMACAddress);
+                                String timestamp = sdf.format(new Timestamp(System.currentTimeMillis()));
+                                System.out.println(timestamp + " - Welcome home! " + bleDeviceNames.get(bleMACAddress));
+                                try {
+                                    if (sendPingRequest(lightIPAddress)) {
+                                        Light currentState = getLight();
+                                        if (Objects.equals(currentState, Light.OFF)) {
+                                            if (!Objects.equals(currentState, living_roomMap.get(bleMACAddress))) {
+                                                setLight(Light.TOGGLE);
+                                            }
+                                        }
+                                    }
+                                } catch (IOException e) {
+                                }
+                            }
+                        }
+                    }
                 } catch (SerialPortTimeoutException se) {
+//                    System.out.println("SerialPortTimeoutException ..."); // TODO Remove after testing
                 }
                 if (requireUpdatesFromDatabase(conn)) {
                     emptyMaps();
@@ -78,6 +106,7 @@ public class App
             }
         } catch (Exception e) {
             System.err.println("** No Serial Input **");
+            e.printStackTrace();
         }
 
         // Close JDBC connection to my MySQL database
@@ -85,6 +114,35 @@ public class App
 
         // Close Serial Port
         commPort.closePort();
+    }
+
+    private static void sendAll(SerialPort commPort)
+    {
+        for (String bleDevice : bleDeviceNames.keySet()) {
+            String bleDeviceInfoToSend = bleDevice + "+" + bleDeviceNames.get(bleDevice) + "*";
+            commPort.writeBytes(bleDeviceInfoToSend.getBytes(), bleDeviceInfoToSend.length());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("** Sent BLE Device Info to IoT Device **");
+        infoSent = true;
+    }
+
+    // Check for BLE IPv6 MAC Address
+    private static String checkIsIPv6MACAddress(String input)
+    {
+        Matcher m2 = VALID_IPV6_PATTERN.matcher(input);
+        boolean result = m2.find();
+        if (result) {
+//            System.out.println("** Found IPv6 Address **");
+            return m2.group(0);
+        } else {
+//            System.out.println("** Incorrect IPv6 Address **");
+            return "";
+        }
     }
 
     private static Connection connectToDatabase()
@@ -115,6 +173,20 @@ public class App
         }
         rs.close();
         stmt.close();
+    }
+
+    // Sends ping request to a provided IP address
+    private static boolean sendPingRequest(String ipAddress) throws UnknownHostException, IOException
+    {
+        InetAddress geek = InetAddress.getByName(ipAddress);
+        System.out.println("Sending Ping request to smart home light: " + ipAddress);
+        if (geek.isReachable(2000)) {
+            System.out.println("Smart home light is reachable\n");
+            return true;
+        } else {
+            System.out.println("Sorry, smart home light is NOT reachable\n");
+            return false;
+        }
     }
 
     private static boolean requireUpdatesFromDatabase(Connection conn) throws SQLException
